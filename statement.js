d54209178e590723da3f020c379b3baaa0b17a31
Abigail Minett <106555227+AbbeyJM@users.noreply.github.com>
2023-01-25 16:08:23 +0000

    statements formatted

--- statement.js
+++ statement.js
@@ -1,3 +1,16 @@
 class Statement {
+    constructor(account) {
+        this.account = account
+    }
 
+    printStatement() {
+        const record = this.account.getRecord()
+        const columns = "date || credit || debit || balance"
+
+        const formatRecord = record.map((transaction) => {
+            return `\n${transaction.date} || ${transaction.type === "credit" ? transaction.amount.toFixed(2) + ' ' : ''}|| ${transaction.type === "debit" ? transaction.amount.toFixed(2) + ' ' : ''}|| ${transaction.balancetoFixed(2)}`
+        })
+        return `${columns}${formatRecord.join('')}`
+    }
+}
 module.exports = Statement
