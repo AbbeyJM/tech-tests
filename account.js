36cb4af0510cc5c6ed483e6460bdcf448ecf369d
Abigail Minett <106555227+AbbeyJM@users.noreply.github.com>
2023-01-25 15:59:21 +0000

    renamed statement to account to allow for transaction statements in own class

---
+++ account.js
@@ -0,0 +1,31 @@
+class Account {
+    constructor() {
+        this.balance = 0
+        this.record = []
+    }
+
+    addDeposit(deposit) {
+        const {amount, date, type} = deposit.returnDeposit()
+
+        if (type === 'credit') {
+            this.balance += amount
+        }
+        else if (type === 'debit') {
+            this.balance -= amount
+        }
+
+        this.record = [{
+            date,
+            amount,
+            type,
+            balance: this.balance,
+        },
+        ...this.record, ]
+        }
+            
+    getRecord() {
+        return this.record
+    }
+}
+
+module.exports = Account
--- statement.js
+++ statement.js
@@ -1,31 +1,3 @@
 class Statement {
-    constructor() {
-        this.balance = 0
-        this.record = []
-    }
-
-    addDeposit(deposit) {
-        const {amount, date, type} = deposit.returnDeposit()
-
-        if (type === 'credit') {
-            this.balance += amount
-        }
-        else if (type === 'debit') {
-            this.balance -= amount
-        }
-
-        this.record = [{
-            date,
-            amount,
-            type,
-            balance: this.balance,
-        },
-        ...this.record, ]
-        }
-            
-    getRecord() {
-        return this.record
-    }
-}
 
 module.exports = Statement
